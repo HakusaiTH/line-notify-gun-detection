eff41e75a780ec1152a0e4b4e452506a499188d1
HakusaiTh <104154862+HakusaiTH@users.noreply.github.com>
2023-03-11 22:26:01 +0700

    Add files via upload

---
+++ gun_track/main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import imutils
+import time
+import requests
+from datetime import datetime
+
+LINE_ACCESS_TOKEN = "Z3nFlOBYdaPDdmLD3FIch5dH35MJywumKXYtjOTLUr5"
+url = "https://notify-api.line.me/api/notify"
+
+location = "location"
+location_gps = "location_gps"
+
+now = datetime.today()
+message = f'{now.strftime("%d/%m/%Y")}   \n' \
+        f'ตรวจพบปืน ที่ {location} เวลา {now.strftime("%I:%M:%S %p")} \n' \
+        f'{location_gps} \n' \
+
+def lineNotify(message,filepath):
+    file = {'imageFile': open(filepath, 'rb')}
+    data = {f'message': {message}}
+    headers = {'Authorization': 'Bearer ' + LINE_ACCESS_TOKEN}
+
+    session = requests.Session()
+    response = session.post(url, headers=headers, data=data, files=file)
+
+gun_cascade = cv2.CascadeClassifier('cascade.xml')
+image = cv2.imread("gun2.jpg")
+
+while True:
+    
+    frame = image.copy()
+
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    gun = gun_cascade.detectMultiScale(gray, 1.3, 5, minSize=(100, 100))
+
+    for (x, y, w, h) in gun:
+
+        frame = cv2.rectangle(frame,(x, y),(x + w, y + h),(255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+    cv2.imshow("Security Feed", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('q'):
+        break
+
+    if len(gun) > 0:
+        print(message)
+        cv2.imwrite('output.jpg', frame)
+        filepath = 'output.jpg'
+        lineNotify(message,filepath)
+        time.sleep(5)
+
+cv2.destroyAllWindows()
